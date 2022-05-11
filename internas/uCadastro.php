d4c60759d150b346fdf06a599df52922d5fed572
Lucas Dias <luca.dias.silva@gmail.com>
2022-05-11 20:55:56 -0300

    senha-esqueceu-senha

---
+++ internas/uCadastro.php
@@ -0,0 +1,25 @@
+<?php	include("header.php");?>
+
+    <main class="main__content_wrapper">
+        
+        <!-- Inicio breadcrumb section -->
+        <section class="breadcrumb__section breadcrumb__bg">
+            <div class="container">
+                <div class="row row-cols-1">
+                    <div class="col">
+                        <div class="breadcrumb__content text-center">
+                            <h1 class="breadcrumb__content--title text-white mb-25">Cadastro</h1>
+                            <ul class="breadcrumb__content--menu d-flex justify-content-center">
+                                <li class="breadcrumb__content--menu__items"><a class="text-white" href="<?= $path["site"] ?>">Página inicial</a></li>
+                                <li class="breadcrumb__content--menu__items"><span class="text-white">Cadastro</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--/fim breadcrumb section -->
+        
+    </main>
+
+<?php	include("footer.php");?>
---
+++ internas/uEsqueceuSenha.php
@@ -0,0 +1,26 @@
+<?php	include("header.php");?>
+
+    <main class="main__content_wrapper">
+        
+        <!-- Inicio breadcrumb section -->
+        <section class="breadcrumb__section breadcrumb__bg">
+            <div class="container">
+                <div class="row row-cols-1">
+                    <div class="col">
+                        <div class="breadcrumb__content text-center">
+                            <h1 class="breadcrumb__content--title text-white mb-25">Esqueceu senha</h1>
+                            <ul class="breadcrumb__content--menu d-flex justify-content-center">
+                                <li class="breadcrumb__content--menu__items"><a class="text-white" href="<?= $path["site"] ?>">Página inicial</a></li>
+                                <li class="breadcrumb__content--menu__items"><a class="text-white" href="<?= $path["site"] ?>login">Login</a></li>
+                                <li class="breadcrumb__content--menu__items"><span class="text-white">Esqueceu senha</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--/fim breadcrumb section -->
+        
+    </main>
+
+<?php	include("footer.php");?>
