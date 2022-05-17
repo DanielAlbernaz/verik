9f4da7eb50a4cc9b8ac792b8ddff0bfdb96cc436
ThalesAlbernaz <89394976+ThalesAlbernaz@users.noreply.github.com>
2022-05-17 18:38:53 -0300

    Termo de uso

--- painel/geral/ctrlTermo.php
+++ painel/geral/ctrlTermo.php
@@ -10,7 +10,9 @@
 include_once "classes/class.Termo.php";
 $objTermo = new Termo();
 
-$permissao = $objSecao->permissaoSecaoFixaUsuario("39",$objSession2->get('tlAdmLoginId'));
+include_once "classes/class.Crop_Imagem.php";
+
+$permissao = $objSecao->permissaoSecaoFixaUsuario("17",$objSession2->get('tlAdmLoginId'));
 
 //verifica qual a ação está sendo solicitada pela câmada de visão(formulários)
 switch ($objPost->param['acao']) {
@@ -21,7 +23,7 @@ case "frmAlterar":
 		'id' => $objPost->param["id"]
 	);
 	$termoForm = $objTermo->lista($condicao);
-	$objUteis->encode($termoForm);
+	// $objUteis->encode($termoForm);
 	// inclui o formulario
 	$abrePag = "../frms/frmAltTermo.php";
 break;
@@ -31,14 +33,63 @@ case "alterar":
 	$form['id'] = $objPost->param['id'];
 	$form['titulo'] = $objPost->param['titulo'];
 	$form['texto'] = $objPost->param['texto'];	
-	$form['status'] = 1;
+
+	
+	//se tiver selecionado uma imagem destaque
+	if($objPost->param["imagem"]["name"] !=""){
+		$formatoImgDestaque = ".".$objUteis->formatoFile($objPost->param["imagem"]["name"]);
+		if($formatoImgDestaque == ".jpg" || $formatoImgDestaque == ".JPG" || $formatoImgDestaque == ".jpeg" || $formatoImgDestaque == ".JPEG" || $formatoImgDestaque == ".png" || $formatoImgDestaque == ".PNG" || $formatoImgDestaque == ".gif" || $formatoImgDestaque == ".GIF") {
+
+		}else{
+				$objUteis->showResult("","Formato de arquivo inválido. Apenas imagens .jpg, png, gif ou .jpeg",false,"mostraMensagem",'index.php?acao=alterar&ctrl=noticias');
+				exit();
+		}
+
+		//Retorna formato da imagem
+		$formatoImgDestaque = $objUteis->formatoFile($objPost->param["imagem"]["name"]);
+		//Definir nome para imagem
+		$dir = "arq_termo/";
+		if(!file_exists("arq_termo")) {
+				$objUteis->criaDir("arq_termo");
+		}
+		$nomeImg = "termo".time().".".$formatoImgDestaque;
+		$temp = $dir.$nomeImg;
+			//deleta a imagem antiga
+			$objUteis->delFile($objPost->param['imgAntiga']);
+			//Fazendo o upload da imagem
+					$imagem = $objPost->param["imagem"];
+					// armazena dimensões da imagem
+					$imagesize = getimagesize($imagem['tmp_name']);				
+					if($imagesize !== false){
+						// move a imagem para o servidor
+						if($objUteis->uploadArq($objPost->param["imagem"]["tmp_name"],$temp)){
+							$oImg = new Crop_Imagem($temp );
+							// valida via m2brimagem
+							if($oImg->valida() == 'OK'){					
+								// redimensiona (caso seja menor que o tamanho )
+								$oImg->redimensiona('1920', '', '');
+								// grava nova imagem
+								$oImg->grava($temp);                            
+								$oImg->posicaoCrop( $_POST['x']*2, $_POST['y']*2 );
+								$oImg->redimensiona( $_POST['w']*2, $_POST['h']*2, 'crop' );
+								$oImg->redimensiona('1920', '', '');
+								$oImg->grava( $temp );
+							}
+						}
+					}
+					$imgDestaque = $dir.$nomeImg;
+					$form['imagem'] = $imgDestaque;
+			}else{
+			$form['imagem'] = $objPost->param['imgAntiga'];
+			}
+				
 	
 	
 	//se tiver selecionado uma imagem
 	
 
 	//altera o registro no banco
-	$objUteis->decode($form);
+	// $objUteis->decode($form);
 	$result = $objTermo->alterar($form);
 	
 	//verifica se foi alterado
@@ -46,7 +97,7 @@ case "alterar":
 		//grava log de ação no sistema
 		$objUteis
 				->gravaLog($objSession2->get('tlAdmLoginNome'),
-						utf8_decode("Termo"),
+						utf8_decode("termo"),
 						$objSession2->get('tlAdmLoginId'), "Alterou",
 						$_SERVER['REMOTE_ADDR']);
 	}
--- painel/geral/frms/frmAltTermo.php
+++ painel/geral/frms/frmAltTermo.php
@@ -17,13 +17,17 @@ $objForm->sk_formText('Título','titulo','',true,'Aqui digita um título.', $ter
 
 $objForm->sk_formTextHTML('Texto','texto',true,'Aqui você escreve o texto.', $termoForm->texto);
 
+$objForm->sk_formFileCrop("Imagem",'imagem',false,'Tamanho da imagem do banner 1920px x 400px',$termoForm->imagem);
 
 //Cria um input hidden
 $objForm->sk_formHidden('id',$termoForm->id);
 
 
+//Cria um input hidden
+$objForm->sk_formHidden('imgAntiga',$termoForm->imagem);
+
 //Verfica se o usuário e Administrador
-if($permissao->alterar == 1){
+if($permissao->alterar == 1){   
     //Cria um input submit
     $objForm->sk_formSubmit();
 }
@@ -32,3 +36,52 @@ if($permissao->alterar == 1){
 $objForm->sk_fimDoFormulario();
 
 ?>
+
+
+
+<!-- Script para recorte da imagem. É necessário colocar esse script em todos os formulários que forem utilizar recorte para que seja informado o tamanho da imagem -->
+<script>
+
+            $( "#img" ).click(function() {
+                $("#jcrop").remove();
+                $(".jcrop-holder").remove();
+                $(".imgPreview .formRight").append("<img id='jcrop'/>");
+            });
+
+            function preview(input){
+                if (input.files && input.files[0]){
+                    var reader = new FileReader(); 
+                    reader.onload = function(e){
+                        $('#jcrop').attr('src', e.target.result).width(1920/2),
+                        cropImg()
+                    };
+                    reader.readAsDataURL(input.files[0]);                    
+                    $(".imgPreview").show();
+                }
+            }; 
+
+            function cropImg(){        
+                $('#jcrop').Jcrop({
+                    onChange: exibePreview,
+                    onSelect: exibePreview,
+                    bgColor         : 'white',
+                    minSize         : [ 100, 100 ],
+                    maxSize         : [ 1920/2, 400/2 ],
+                    setSelect       : [ 0, 0, 1920/2, 400/2 ],                       
+                    bgOpacity       : .3,
+                    borderOpacity   : .9,
+                    allowResize	: true,
+                    aspectRatio: 1920/400
+                });        
+            };
+
+            function exibePreview(c){            
+                $('#x').val(c.x);
+                $('#y').val(c.y);
+                $('#x2').val(c.x2);
+                $('#y2').val(c.y2);
+                $('#w').val(c.w);
+                $('#h').val(c.h);
+            };    
+
+</script>
